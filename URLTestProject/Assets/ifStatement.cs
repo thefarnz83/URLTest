5466ed82ce6ed656fbef66b94316a81357627c45
Michael Farnsworth <thefarnz83@gmail.com>
2015-09-24 09:05:59 -0600

    If Statement script and scene

--- URLTestProject/Assets/ifStatement.cs
+++ URLTestProject/Assets/ifStatement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour {
+public class ifStatement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
